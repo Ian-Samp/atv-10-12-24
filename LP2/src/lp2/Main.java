e5f02507cec0de1aae918aae835b987da7597720
Ian-Samp <ianpsmoura21@gmail.com>
2024-12-11 20:18:32 -0300

    Add files via upload

---
+++ LP2/src/lp2/Main.java
@@ -0,0 +1,12 @@
+package lp2;
+
+public class Main {
+	
+	public static void main(String [] args) {
+		Calculo c = new Calculo();
+		Funcionario f = new Funcionario("Emerson", "11/12/24", 0, 2, 16);
+
+		System.out.println(f.imprimirDados());
+	}
+	
+}
